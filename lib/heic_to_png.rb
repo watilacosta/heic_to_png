c2cb6f7ff533101b0d2b2f08703d9bd3e56a78b6
Watila Costa <watila.costa@redealumni.com>
2023-02-06 11:03:13 -0300

    first commit

---
+++ lib/heic_to_png.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "heic_to_png/version"
+
+module HeicToPng
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "heic_to_png"
+
+require "minitest/autorun"
